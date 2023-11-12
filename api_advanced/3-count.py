e1c9d9e0fe5869165a87cf47da1c099257ba7c61
kuir-juach <k.thuch@alustudent.com>
2023-11-12 06:30:30 -0800

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Query for the reddit api and parse the title of all hot articles
-"""
-
+""" 3-count.py """
 import json
 import requests
 
 
 def count_words(subreddit, word_list, after="", count=[]):
+    """ prints a sorted count of given keywords """
 
     if after == "":
         count = [0] * len(word_list)
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url,
-                            params={'after': after},
-                            allow_redirects=False,
-                            headers={'user-agent': 'bhalut'})
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'User-Agent': 'Mozilla/5.0'})
 
-    if response.status_code == 200:
-        data = response.json()
+    if request.status_code == 200:
+        data = request.json()
 
         for topic in (data['data']['children']):
             for word in topic['data']['title'].split():
